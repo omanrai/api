82cb190ef03bdddbd6ed533f4602bda6876f5d29
omanrai <oman2023rai@gmail.com>
2024-03-03 00:04:31 +0545

    "connected"

--- lib/model/service.dart
+++ lib/model/service.dart
@@ -10,4 +10,6 @@ class Service {
 
     return products;
   }
+
+
 }
--- lib/view/addProduct.dart
+++ lib/view/addProduct.dart
@@ -1,4 +1,11 @@
+import 'dart:html';
+
+import 'package:api/model/service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 
 class CreateProduct extends StatefulWidget {
   const CreateProduct({super.key});
@@ -8,13 +15,76 @@ class CreateProduct extends StatefulWidget {
 }
 
 class _CreateProductState extends State<CreateProduct> {
+  GlobalKey<FormState> key = GlobalKey<FormState>();
+  TextEditingController name = TextEditingController();
+  TextEditingController price = TextEditingController();
+  TextEditingController description = TextEditingController();
+
+  void createItem() {
+    String urlCreate = "http://192.168.1.1:8080/api/createproduct.php";
+    http.post(Uri.parse(urlCreate),
+        body: {'a': name, 'b': price, 'c': description});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Create Product')),
       body: SingleChildScrollView(
         child: Form(
-          child: Column(),
+          key: key,
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: name,
+                  decoration: InputDecoration(
+                    labelText: 'Item Name',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) =>
+                      value!.isEmpty ? 'Please enter an item name' : null,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: price,
+                  decoration: InputDecoration(
+                    labelText: 'Price',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) =>
+                      value!.isEmpty ? 'Please enter an price name' : null,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: description,
+                  decoration: InputDecoration(
+                    labelText: 'Description',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) =>
+                      value!.isEmpty ? 'This field cannot be empty' : null,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (key.currentState!.validate()) {
+                      createItem();
+                      print("record saved");
+                    }
+                  },
+                  child: Text("create new prodct"),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
